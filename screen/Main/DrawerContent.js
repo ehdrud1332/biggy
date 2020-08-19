65360a9d30613d25e98afc502b7b864c3d43d57c
CastalJaemin <ehdrud1332@gmail.com>
2020-08-20 01:21:06 +0900

    add DrawerContent
    Drawer 만들기

--- screen/Main/DrawerContent.js
+++ screen/Main/DrawerContent.js
@@ -1,10 +1,139 @@
 import React from 'react';
-import {View, Text, StatusBar} from 'react-native';
+import {View} from 'react-native';
+import styled from 'styled-components/native';
+import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
+import {
+    Avatar,
+    Paragraph,
+    Drawer,
+    TouchableRipple,
+} from 'react-native-paper';
+import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 
-const DrawerContent = () => {
+const DrawerCon = styled.View`
+  flex: 1;
+`;
+
+const UserInfoSection = styled.View`
+  padding-left: 20px;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  margin-top: 3px;
+  font-weight: 600;
+`;
+
+const Caption = styled.Text`
+  font-size: 12px;
+  line-height: 15px;
+  color: #303030;
+`;
+
+const Row  = styled.View`
+   margin-top: 20px;
+   flex-direction: row;
+   align-items: center;
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-right: 15px;
+`;
+
+const DrawerContent = (props) => {
     return (
-        <View>
-            <Text>DrawerContent</Text>
+        <View style={{flex: 1}}>
+            <DrawerContentScrollView {...props}>
+                <DrawerCon>
+                    <UserInfoSection>
+                        <View style={{flexDirection: 'row', marginTop: 20}}>
+                            <Avatar.Image
+                                size={50}
+                            />
+                            <View style={{marginLeft: 15, flexDirection: 'column'}}>
+                                <Title>JaeminSeong</Title>
+                                <Caption>@ehdrud1332</Caption>
+                            </View>
+                        </View>
+
+                        <Row>
+                            <Section>
+                                <Paragraph
+                                    style={{
+                                        fontWeight: 'bold',
+                                        marginRight: 3,
+                                        fontSize: 14,
+                                        lineHeight: 14
+                                    }}
+                                >80</Paragraph>
+                                <Caption>Following</Caption>
+                            </Section>
+
+                            <Section>
+                                <Paragraph
+                                    style={{
+                                        fontWeight: 'bold',
+                                        marginRight: 3,
+                                        fontSize: 14,
+                                        lineHeight: 14
+                                    }}
+                                >100</Paragraph>
+                                <Caption>Followers</Caption>
+                            </Section>
+                        </Row>
+
+                        <Drawer.Section style={{
+                            marginTop: 15
+                        }}>
+                            <DrawerItem
+                                icon={() => (
+                                    <Icon
+                                        name="bookmark"
+                                        size={26}
+                                    />
+                                )}
+                                label="Bookmark"
+                                onPress={() => {props.navigation.navigate("Bookmark")}}
+                            />
+
+                        </Drawer.Section>
+
+                        <Drawer.Section style={{
+                            marginTop: 15
+                        }}>
+                            <DrawerItem
+                                icon={() => (
+                                    <Icon
+                                        name="account-outline"
+                                        size={26}
+                                    />
+                                )}
+                                label="Profile"
+                                onPress={() => {props.navigation.navigate("Bookmark")}}
+                            />
+
+                        </Drawer.Section>
+
+                        <Drawer.Section style={{
+                            marginTop: 15
+                        }}>
+                            <DrawerItem
+                                icon={() => (
+                                    <Icon
+                                        name="account-check-outline"
+                                        size={26}
+                                    />
+                                )}
+                                label="Support"
+                                onPress={() => {props.navigation.navigate("Bookmark")}}
+                            />
+
+                        </Drawer.Section>
+                    </UserInfoSection>
+                </DrawerCon>
+            </DrawerContentScrollView>
         </View>
     );
 };
--- screen/Main/HomeScreen.js
+++ screen/Main/HomeScreen.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import {View, Text, StatusBar} from 'react-native';
+import {StatusBar} from 'react-native';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+
+const T1 = styled.Text`
+  font-weight: bold;
+  font-size: 30px;
+`;
+
 
 
 const HomeScreen = () => {
     return (
-        <View>
+        <Container>
             <StatusBar backgroundColor="#009387" barStyle="light-content"/>
-            <Text>HomeScreen</Text>
-        </View>
+            <T1>대희쌉변태</T1>
+        </Container>
     );
 };
 
