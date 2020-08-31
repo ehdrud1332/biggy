501132bc8b5944b02c6e2634ec1548f5eab10b74
CastalJaemin <ehdrud1332@gmail.com>
2020-09-01 00:51:54 +0900

    MainTab Update blackCustom
    검은색  TabBar로 update

---
+++ screen/Main/ProfileScreen2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, Button, ScrollView} from 'react-native';
+
+const MyComponent = () => {
+    return (
+        <ScrollView>
+
+        </ScrollView>
+    );
+};
+
+export default MyComponent;
--- screen/MainTabScreen.js
+++ screen/MainTabScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 
 import {Ionicons} from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome'
@@ -10,27 +9,31 @@ import HotelScreen from "./Main/HotelScreen";
 import ExploreScreen from "./Main/ExploreScreen";
 import ProfileScreen from "./Main/ProfileScreen";
 import HomeStackScreen from "./Stack/HomeStackScreen";
+import ProfileScreen2 from './Main/ProfileScreen2';
 
-const Tab = createMaterialBottomTabNavigator();
-
-const HomeStack = createStackNavigator();
-const DetailStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const MainTabScreen = () => (
     <Tab.Navigator
         initialRouteName="Home"
-        activeColor="#fff"
+        tabBarOptions={{
+            inactiveBackgroundColor: "#000000",
+            activeBackgroundColor: "#000000",
+            activeTintColor: "#fff",
+            inactiveTintColor: "#9b9b9b",
+            labelStyle: {fontWeight: '600', textTransform: "uppercase"}
+        }}
+
     >
         <Tab.Screen
             name="Home"
             component={HomeStackScreen}
             options={{
-                tabBarLabel: "Home",
-                tabBarColor: "black",
-                tabBarIcon: () => (
-                    <Ionicons name="ios-home"
-                          color="white"
-                          size={26}
+                tabBarIcon: ({focused}) => (
+                    <Ionicons
+                        name="ios-home"
+                        color= {focused ? "#fff" : "#9b9b9b"}
+                        size={26}
                     />
                 )
             }}
@@ -40,12 +43,11 @@ const MainTabScreen = () => (
             name="Hotel"
             component={HotelScreen}
             options={{
-                tabBarLabel: "Hotel",
-                tabBarColor: "black",
-                tabBarIcon: () => (
-                    <FontAwesome name="hotel"
-                          color="white"
-                          size={26}
+                tabBarIcon: ({focused}) => (
+                    <FontAwesome
+                        name="hotel"
+                        color= {focused ? "#fff" : "#9b9b9b"}
+                        size={26}
                     />
                 )
             }}
@@ -55,12 +57,25 @@ const MainTabScreen = () => (
             name="Explore"
             component={ExploreScreen}
             options={{
-                tabBarLabel: "Explore",
-                tabBarColor: "black",
-                tabBarIcon: () => (
-                    <FontAwesome name="search"
-                          color="white"
-                          size={26}
+                tabBarIcon: ({focused}) => (
+                    <FontAwesome
+                        name="search"
+                        color= {focused ? "#fff" : "#9b9b9b"}
+                        size={26}
+                    />
+                )
+            }}
+        />
+
+        <Tab.Screen
+            name="Profile2"
+            component={ProfileScreen2}
+            options={{
+                tabBarIcon: ({focused}) => (
+                    <Ionicons
+                        name="md-person"
+                        color= {focused ? "#fff" : "#9b9b9b"}
+                        size={26}
                     />
                 )
             }}
@@ -70,12 +85,11 @@ const MainTabScreen = () => (
             name="Profile"
             component={ProfileScreen}
             options={{
-                tabBarLabel: "Profile",
-                tabBarColor: "black",
-                tabBarIcon: () => (
-                    <Ionicons name="md-person"
-                          color="white"
-                          size={26}
+                tabBarIcon: ({focused}) => (
+                    <Ionicons
+                        name="md-person"
+                        color= {focused ? "#ffffff" : "#9b9b9b"}
+                        size={26}
                     />
                 )
             }}
